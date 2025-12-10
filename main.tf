2b730eee4d328d7e4e2f59d6bea1fa82ded20bcc
h3ow3d <holdens.uk@proton.me>
2025-12-10 19:28:31 +0000

    init

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Main resource definitions go here
+
+# Example:
+# resource "aws_example" "main" {
+#   name = "${var.project_name}-${var.environment}-example"
+#   
+#   tags = merge(var.tags, {
+#     Name = "${var.project_name}-${var.environment}-example"
+#   })
+# }
